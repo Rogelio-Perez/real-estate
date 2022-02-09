e61ecfefddc2d2360cfe331ea7251ebd5803e580
roger <perezmenarogelio@gmail.com>
2022-02-09 15:07:42 -0600

    search page

---
+++ components/SearchFilters.jsx
@@ -0,0 +1,19 @@
+import { useEffect, useState } from "react"
+import {
+  Flex,
+  Select,
+  Box,
+  Input,
+  Spinner,
+  Icon,
+  Button,
+} from "@chakra-ui/react"
+import { useRouter } from "next/router"
+import { MdCancel } from "react-icons/md"
+import Image from "next/image"
+
+const SearchFilters = () => {
+  return <div>searchFilters</div>
+}
+
+export default SearchFilters
